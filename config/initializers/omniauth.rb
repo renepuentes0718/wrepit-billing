b0e5e370cab48b557e6bc10e6c03d7b622624d93
addo forison <boakye.addo@tradelink.co>
2024-07-05 04:55:35 +0000

    update Omniauth options

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+    scope: 'email, profile',
+    image_aspect_ratio: 'square',
+    image_size: 50,
     provider_ignores_state: true,
     prompt: 'select_account consent',
     callback_path: '/auth/google_oauth2/callback'
